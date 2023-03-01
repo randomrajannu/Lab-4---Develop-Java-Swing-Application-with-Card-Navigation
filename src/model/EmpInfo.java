9087a519ccab3d9c32111b605c9d8b226cfa7cb1
randomrajannu <panchal.raj@northeastern.edu>
2023-03-01 12:41:22 -0500

    commit all

---
+++ src/model/EmpInfo.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author RAJAN
+ */
+public class EmpInfo {
+    
+    private String name;
+    private int employeeId;
+    private int age;
+    private String gender;
+    private String startDate;
+    private String level;
+    private String teamInfo;
+    private String positionTitle;
+    private int phoneNumber;
+    private String email;
+    private Image Photo;
+
+    public Image getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(Image Photo) {
+        this.Photo = Photo;
+    }
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
+}
---
+++ src/model/EmpInfoDatabase.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author RAJAN
+ */
+public class EmpInfoDatabase {
+    
+    private ArrayList<EmpInfo> database;
+    
+    public EmpInfoDatabase(){ // constructor for EmpInfoDatabase
+        
+        this.database = new ArrayList<EmpInfo>();    
+    } 
+
+    public ArrayList<EmpInfo> getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(ArrayList<EmpInfo> database) {
+        this.database = database;
+    }
+
+     public EmpInfo addNewEmp(){
+         
+         EmpInfo newEmp = new EmpInfo();
+         database.add(newEmp);
+         return newEmp;
+     } 
+     
+     public void deleteEmpInfo(EmpInfo  ei){
+         database.remove(ei);
+     }
+}
